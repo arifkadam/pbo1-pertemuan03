c68b8ea6155152a13cb6bc068d7080336bdf984f
arifkadam <62461132+arifkadam@users.noreply.github.com>
2020-04-13 19:15:13 +0700

    Statement1

---
+++ src/Percabangan/IfElse.java
@@ -0,0 +1,14 @@
+package Percabangan;
+
+public class IfElse {
+    public static void main(String[] args) {
+        double totalBelanja = 25000;
+        double uangDiDompet = 50000;
+
+        if(uangDiDompet<totalBelanja){
+            System.out.println("Uang Kurangi jajan anda");
+        }else{
+            System.out.println("Uang cukup, berelaan lah");
+        }
+    }
+}
