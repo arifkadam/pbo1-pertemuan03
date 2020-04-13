d986500c0579ad9df5942fefc4cce528a9f9772e
arifkadam <62461132+arifkadam@users.noreply.github.com>
2020-04-13 19:44:13 +0700

    percabanganswitchdeganChar

---
+++ src/Percabangan/switchChar.java
@@ -0,0 +1,29 @@
+package Percabangan;
+
+public class switchChar {
+    public static void main(String[] args) {
+        char nilai='D';
+        switch (nilai){
+            case 'A':
+                System.out.println("Mhs : \"Terima kasih pak\"" );
+                System.out.println("Dosen : \"Selamat ya\"");
+                break;
+            case 'B':
+                System.out.println("Mhs : \"Kenapa ulun kada dapat A, Pak\"" );
+                System.out.println("Dosen : \"!@#$%\"");
+                break;
+            case 'C':
+                System.out.println("Mhs : \"Ulun turun pul pak ai, tugas pul jua\"" );
+                System.out.println("Dosen : \"Tapi bisa lah menjawab ujian\"");
+                System.out.println("Mhs : \"hihihi...\"");
+                break;
+            case 'D':
+                System.out.println("Mhs : \"Ulun turun pul pak ai, tugas pul jua\"" );
+                System.out.println("Dosen : \"Bujur jua kah\"");
+                System.out.println("Dosen : \"Memeriksa berkas\"");
+                System.out.println("Dosen : \"Menceleng\"");
+                System.out.println("Mhs :\"kabur..\"");
+                break;
+        }
+    }
+}
